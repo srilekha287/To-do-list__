b1995fdbe685be07c134d1b8221fb4d42655ed8f
Srilekha <srilekhapandla28@gmail.com>
2025-05-18 15:23:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ app.put('/edit-item',(req,res)=>{
 const db=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'Mysql@1234',
+    password:'abcd@1234',
     database:'todo'
 })
 
